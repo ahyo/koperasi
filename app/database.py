8281081b99a10d13afccdfd560f8eaec834a2de7
Ahyo Haryanto <ahyo@Mac.local>
2025-10-16 16:53:08 +0700

    update

--- app/database.py
+++ app/database.py
@@ -2,17 +2,20 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from app.config import settings
 
-engine = create_engine(
-    settings.DATABASE_URL,
-    connect_args={"check_same_thread": False} if settings.DATABASE_URL.startswith("sqlite") else {},
-    echo=False,
-    future=True,
-)
+# engine = create_engine(
+#     settings.DATABASE_URL,
+#     connect_args={"check_same_thread": False} if settings.DATABASE_URL.startswith("sqlite") else {},
+#     echo=False,
+#     future=True,
+# )
+engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+# SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False, future=True)
 
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False, future=True)
 
 class Base(DeclarativeBase):
     pass
+
 
 def get_db():
     db = SessionLocal()
